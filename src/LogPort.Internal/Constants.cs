93b76f7eb56fd866eb072e3f7338f94d40b33d3c
Thiago Vasconcelos <thiagomvas@gmail.com>
2026-01-05 18:10:18 -0300

    feat(metrics): track docker logs read globally and per container

---
+++ src/LogPort.Internal/Constants.cs
@@ -0,0 +1,19 @@
+namespace LogPort.Internal;
+
+public static class Constants
+{
+    public static class Metrics
+    {
+        public const string LogsProcessed = "logs.processed";
+        public const string DockerLogsRead = "docker.logs.read";
+        public static string BuildDockerLogsReadKeyForContainer(string container)
+            => $"{DockerLogsRead}.{Normalize(container)}";
+
+        private static string Normalize(string value)
+            => value
+                .TrimStart('/')
+                .ToLowerInvariant();
+
+    }
+    
+}
