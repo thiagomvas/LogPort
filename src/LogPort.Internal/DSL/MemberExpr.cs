d7199d60fe9b2f3ce54275878770d21527c93750
Thiago Vasconcelos <thiagomvas@gmail.com>
2026-01-23 17:17:20 -0300

    chore: Fix formatting

--- src/LogPort.Internal/DSL/MemberExpr.cs
+++ src/LogPort.Internal/DSL/MemberExpr.cs
@@ -1,4 +1,3 @@
 namespace LogPort.Internal.DSL;
 
 public sealed record MemberExpr(Expr Target, string Member) : Expr;
-
--- src/LogPort.Internal/DSL/MetadataKeyExpr.cs
+++ src/LogPort.Internal/DSL/MetadataKeyExpr.cs
@@ -1,4 +1,3 @@
 namespace LogPort.Internal.DSL;
 
 public sealed record MetadataKeyExpr(string Key) : Expr;
-
